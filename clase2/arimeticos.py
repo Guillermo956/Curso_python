b1c5cfaa7da97fc3269d623eec3107b3322c1958
Guillermo_Zorrilla <undertale3544@gmail.com>
2025-06-28 12:59:16 -0300

    operadores arimeticos

---
+++ clase2/arimeticos.py
@@ -0,0 +1,19 @@
+'''
+operaciones arimeticas basicas en python
+sumamos, restamos, multiplicamos y dividimos dos numeros
+'''
+a = 17
+b = 18
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+h = a ** b
+print("suma",c)
+print("resta",d)
+print("multiplicacion",e)
+print("division",f)
+print("modulo",g)
+print("Exponente",h)
+#operadores de asignacion
