cf39dc0cdd1d87693436580fed91bc1ae3b674da
Guillermo_Zorrilla <undertale3544@gmail.com>
2025-06-28 12:53:22 -0300

    finalizado operadores

---
+++ clase2/operadores.py
@@ -0,0 +1,23 @@
+'''}
+operadores
+a == b
+a != c
+a <  b
+a >  b 
+a <= b
+a >= b
+'''
+a=17
+b=18
+r=a == b
+print(r)
+r=a != b
+print(r)
+r=a < b
+print("es mayor",r)
+r=a > b
+print("es menor",r)
+r=a <= b
+print(r)
+r=a >= b
+print(r)
