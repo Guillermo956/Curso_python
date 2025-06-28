fa6e00b03b19990068afa9b9ca51dd7fb29bf190
Guillermo_Zorrilla <undertale3544@gmail.com>
2025-06-28 12:42:28 -0300

    finalizado variable

---
+++ clase2/variables.py
@@ -0,0 +1,34 @@
+'''
+esto es un comentario
+Occaecat elit consequat veniam commodo sit fugiat ex eu et do. Adipisicing qui et tempor velit officia magna officia. Est incididunt in in officia tempor non officia Lorem aliqua ea veniam nisi Lorem. Duis ut mollit qui do cillum exercitation aliqua pariatur aute non ut dolore eu. Amet proident ipsum cupidatat fugiat qui. Amet aliqua incididunt occaecat veniam voluptate officia laborum culpa culpa cillum ea deserunt elit. Culpa dolore consequat tempor occaecat cupidatat consectetur nulla aliquip labore est reprehenderit ex.
+
+Reprehenderit sit veniam laborum cupidatat cillum esse tempor qui velit esse mollit dolor veniam amet. Duis mollit consectetur minim culpa nisi labore consectetur. Eu ullamco sunt in in cillum nisi dolor. Proident dolor occaecat aliquip quis eiusmod quis ex mollit cillum labore dolore excepteur fugiat. Nostrud duis qui sunt ex laboris.
+
+Exercitation enim voluptate proident culpa tempor ut ea officia ea aliquip laboris sit. Velit eu aliqua occaecat veniam aliquip aliqua in ut cillum proident esse. Nulla sunt consectetur nostrud ex occaecat enim velit. Esse excepteur magna mollit irure do in reprehenderit laboris commodo aliquip aliqua esse ad ad. Aute ad mollit ipsum proident sunt.
+
+Duis elit deserunt ex nisi amet ex occaecat dolor cillum. Deserunt nulla irure aute aute ex tempor ipsum nostrud consectetur pariatur laborum et tempor officia. Non ipsum ullamco nulla veniam. Cupidatat dolor occaecat sunt enim ipsum magna consequat reprehenderit fugiat elit adipisicing. Elit ut minim nostrud elit Lorem est labore labore esse amet nostrud occaecat. Laboris commodo aliquip nostrud dolor reprehenderit. Incididunt ut ullamco et ut sunt culpa minim cillum dolor quis.
+
+Elit Lorem non nostrud irure ipsum ex. Quis fugiat est ex officia qui esse nisi irure pariatur consectetur. Est non dolor esse commodo laboris qui ea consectetur sit. Aute quis exercitation nisi nostrud sit voluptate et voluptate pariatur in esse esse est. Dolor pariatur mollit et velit consequat. Id elit deserunt exercitation aliquip nostrud. Officia duis magna velit sit aliqua exercitation voluptate tempor amet.
+
+Enim occaecat minim consequat officia reprehenderit labore dolor velit elit aliquip aliqua ea excepteur. Aute eu esse aliquip labore enim. Sint anim commodo ullamco ut esse occaecat dolor quis minim elit ea nostrud pariatur. Lorem sit incididunt esse ullamco nulla voluptate amet. Sint Lorem et enim elit eiusmod ullamco sit sint.
+
+Fugiat tempor qui qui culpa ullamco. Dolore occaecat exercitation occaecat proident cupidatat veniam excepteur et Lorem officia ut magna sint. Sint sint fugiat cupidatat consequat ipsum cupidatat ullamco duis esse consectetur cillum voluptate. Est esse enim Lorem ullamco. Veniam voluptate tempor magna veniam enim esse esse ea officia exercitation proident amet. Reprehenderit eu minim exercitation irure commodo amet cupidatat commodo quis occaecat esse proident cillum. Fugiat ea aliquip cupidatat consequat proident.
+
+Veniam velit eiusmod qui veniam dolore elit cillum enim sint deserunt ad. Et commodo dolor do nulla aute ullamco ullamco. Consequat cillum excepteur nisi ipsum excepteur mollit pariatur et. Ea cillum veniam irure voluptate dolor consectetur sunt consequat deserunt ipsum. Non in aliquip duis occaecat elit exercitation dolore sit nulla aliquip.
+'''
+print("hola mundo")
+
+entero=10
+flotante=10.5
+cadena="hola"
+booleano=True
+print(type(entero),type(flotante),type(cadena),type(booleano))
+
+print("operaciones entre dos numeros")
+a=30
+b=500
+print("suma de dos numeros",a+b)
+print("resta de dos numeros",a-b)
+print("multi de dos numeros",a*b)
+b=2#cambio de variable 
+print("division de dos numeros",a/b)
